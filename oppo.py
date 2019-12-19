3d7485b30df6304181b8ca813bb669cd2ed13698
Anthony Sherwood <iworkinpixels@gmail.com>
2019-12-19 10:11:41 -0600

    Revert Oppo And Make Exponential Envelope In Sine Wav Demo
    Reverts Oppo to the last functional version, and uses the exponential
    table to generate an envelope in the sine wave demo. This is still
    using multiplications and divisions, though, so we need to figure
    out how to set an accumulator or something instead to avoid all those
    cycles being spent calculating the current amplitude values for the
    envelope. We have chased the multiplications out of the main sine
    equation and back into the envelope generator, and once we chase them
    out of there we will have a working NCO.

--- oppo.py
+++ oppo.py
@@ -11,9 +11,9 @@ import time as pytime
 class Voice:
     def __init__(self, op1, op2):
         self.op1 = Operator(0, op1[0], op1[1], op1[2], op1[3], op1[4], op1[5])
-        # self.op2 = Operator(1, op2[0], op2[1], op2[2], op2[3], op2[4], op1[5])
-        # self.op3 = Operator(0, op3[0], op3[1], op3[2], op3[3], op3[4], op3[5])
-        # self.op4 = Operator(1, op4[0], op4[1], op4[2], op4[3], op4[4], op1[5])
+        self.op2 = Operator(1, op2[0], op2[1], op2[2], op2[3], op2[4], op1[5])
+        self.op3 = Operator(0, op3[0], op3[1], op3[2], op3[3], op3[4], op3[5])
+        self.op4 = Operator(1, op4[0], op4[1], op4[2], op4[3], op4[4], op1[5])
         self.note_on = 0.0
         self.note_off = 0.0
         self.z = np.vectorize(self.sampleAt)
@@ -21,35 +21,32 @@ class Voice:
     def __init__(self):
         self.op1 = Operator()
         self.op2 = Operator()
-        # self.op3 = Operator()
-        # self.op4 = Operator()
+        self.op3 = Operator()
+        self.op4 = Operator()
         self.op1.index = 0
         self.op2.index = 1
-        # self.op3.index = 2
-        # self.op4.index = 3
+        self.op3.index = 2
+        self.op4.index = 3
         self.note_on = 0.0
         self.note_off = 0.0
         self.z = np.vectorize(self.sampleAt)
 
     def envLength(self):
-        #return self.op1.a + self.op1.d + 0.25 + self.op1.r
-        return max(self.op1.a, self.op2.a) + max(self.op1.d, self.op2.d) + 0.25 + max(self.op1.r, self.op2.r)
-        # return max(self.op1.a,self.op2.a,self.op3.a,self.op4.a) + max(self.op1.d,self.op2.d,self.op3.a,self.op4.a) + 0.25 + max(self.op1.r,self.op2.r,self.op3.r,self.op4.r)
+        return max(self.op1.a,self.op2.a,self.op3.a,self.op4.a) + max(self.op1.d,self.op2.d,self.op3.a,self.op4.a) + 0.25 + max(self.op1.r,self.op2.r,self.op3.r,self.op4.r)
 
     def sampleAt(self, t):
-        # s = self.op1.sOsc(t, self.note_on, self.note_off)
         s = self.op1.sOscFM(t, self.note_on, self.note_off, self.op2.sOsc(t, self.note_on, self.note_off), self.op2.k)
-        # s += self.op3.sOscFM(t, self.note_on, self.note_off, self.op4.sOsc(t, self.note_on, self.note_off), self.op4.k)
+        s += self.op3.sOscFM(t, self.note_on, self.note_off, self.op4.sOsc(t, self.note_on, self.note_off), self.op4.k)
 
-        if t > self.note_off + self.op1.r + 0.25:
+        if t > self.note_off + max(self.op1.r,self.op3.r) + 0.25:
             self.reset()
         return s
 
     def reset(self):
         self.op1.randomize()
         self.op2.randomize()
-        # self.op3.randomize()
-        # self.op4.randomize()
+        self.op3.randomize()
+        self.op4.randomize()
         new = self.envLength()
         self.note_on = self.note_on + new
         self.note_off = self.note_on + new
@@ -58,29 +55,25 @@ class Voice:
     def dump(self):
         self.op1.dump()
         self.op2.dump()
-        # self.op3.dump()
-        # self.op4.dump()
-        print("RATIO:", round(self.op2.f/self.op1.f, 2))
-        # print("RATIOS:",round(self.op2.f/self.op1.f, 2),round(self.op4.f/self.op3.f, 2))
+        self.op3.dump()
+        self.op4.dump()
+        print("RATIOS:",round(self.op2.f/self.op1.f, 2),round(self.op4.f/self.op3.f, 2))
         print("")
 
 class Operator:
-    q1 = np.pi / 2      # half of pi
-    q2 = np.pi          # pi
-    q3 = np.pi * 1.5    # 1.5 * pi
-    q4 = 2 * np.pi      # tau
-
+    tau = 2 * np.pi
     tableLength = 256
     sinelut=np.array([2137, 1731, 1543, 1419, 1326, 1252, 1190, 1137, 1091, 1050, 1013, 979, 949, 920, 894, 869, 846, 825, 804, 785, 767, 749, 732, 717, 701, 687, 672, 659, 646, 633, 621, 609, 598, 587, 576, 566, 556, 546, 536, 527, 518, 509, 501, 492, 484, 476, 468, 461, 453, 446, 439, 432, 425, 418, 411, 405, 399, 392, 386, 380, 375, 369, 363, 358, 352, 347, 341, 336, 331, 326, 321, 316, 311, 307, 302, 297, 293, 289, 284, 280, 276, 271, 267, 263, 259, 255, 251, 248, 244, 240, 236, 233, 229, 226, 222, 219, 215, 212, 209, 205, 202, 199, 196, 193, 190, 187, 184, 181, 178, 175, 172, 169, 167, 164, 161, 159, 156, 153, 151, 148, 146, 143, 141, 138, 136, 134, 131, 129, 127, 125, 122, 120, 118, 116, 114, 112, 110, 108, 106, 104, 102, 100, 98, 96, 94, 92, 91, 89, 87, 85, 83, 82, 80, 78, 77, 75, 74, 72, 70, 69, 67, 66, 64, 63, 62, 60, 59, 57, 56, 55, 53, 52, 51, 49, 48, 47, 46, 45, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 23, 22, 21, 20, 20, 19, 18, 17, 17, 16, 15, 15, 14, 13, 13, 12, 12, 11, 10, 10, 9, 9, 8, 8, 7, 7, 7, 6, 6, 5, 5, 5, 4, 4, 4, 3, 3, 3, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0])
     explut=np.array([0, 3, 6, 8, 11, 14, 17, 20, 22, 25, 28, 31, 34, 37, 40, 42, 45, 48, 51, 54, 57, 60, 63, 66, 69, 72, 75, 78, 81, 84, 87, 90, 93, 96, 99, 102, 105, 108, 111, 114, 117, 120, 123, 126, 130, 133, 136, 139, 142, 145, 148, 152, 155, 158, 161, 164, 168, 171, 174, 177, 181, 184, 187, 190, 194, 197, 200, 204, 207, 210, 214, 217, 220, 224, 227, 231, 234, 237, 241, 244, 248, 251, 255, 258, 262, 265, 268, 272, 276, 279, 283, 286, 290, 293, 297, 300, 304, 308, 311, 315, 318, 322, 326, 329, 333, 337, 340, 344, 348, 352, 355, 359, 363, 367, 370, 374, 378, 382, 385, 389, 393, 397, 401, 405, 409, 412, 416, 420, 424, 428, 432, 436, 440, 444, 448, 452, 456, 460, 464, 468, 472, 476, 480, 484, 488, 492, 496, 501, 505, 509, 513, 517, 521, 526, 530, 534, 538, 542, 547, 551, 555, 560, 564, 568, 572, 577, 581, 585, 590, 594, 599, 603, 607, 612, 616, 621, 625, 630, 634, 639, 643, 648, 652, 657, 661, 666, 670, 675, 680, 684, 689, 693, 698, 703, 708, 712, 717, 722, 726, 731, 736, 741, 745, 750, 755, 760, 765, 770, 774, 779, 784, 789, 794, 799, 804, 809, 814, 819, 824, 829, 834, 839, 844, 849, 854, 859, 864, 869, 874, 880, 885, 890, 895, 900, 906, 911, 916, 921, 927, 932, 937, 942, 948, 953, 959, 964, 969, 975, 980, 986, 991, 996, 1002, 1007, 1013, 1018])
+    sine = [0.0,0.024541,0.049068,0.073565,0.098017,0.122411,0.14673,0.170962,0.19509,0.219101,0.24298,0.266713,0.290285,0.313682,0.33689,0.359895,0.382683,0.405241,0.427555,0.449611,0.471397,0.492898,0.514103,0.534998,0.55557,0.575808,0.595699,0.615232,0.634393,0.653173,0.671559,0.689541,0.707107,0.724247,0.740951,0.757209,0.77301,0.788346,0.803208,0.817585,0.83147,0.844854,0.857729,0.870087,0.881921,0.893224,0.903989,0.91421,0.92388,0.932993,0.941544,0.949528,0.95694,0.963776,0.970031,0.975702,0.980785,0.985278,0.989177,0.99248,0.995185,0.99729,0.998795,0.999699,1.0,0.999699,0.998795,0.99729,0.995185,0.99248,0.989177,0.985278,0.980785,0.975702,0.970031,0.963776,0.95694,0.949528,0.941544,0.932993,0.92388,0.91421,0.903989,0.893224,0.881921,0.870087,0.857729,0.844854,0.83147,0.817585,0.803208,0.788346,0.77301,0.757209,0.740951,0.724247,0.707107,0.689541,0.671559,0.653173,0.634393,0.615232,0.595699,0.575808,0.55557,0.534998,0.514103,0.492898,0.471397,0.449611,0.427555,0.405241,0.382683,0.359895,0.33689,0.313682,0.290285,0.266713,0.24298,0.219101,0.19509,0.170962,0.14673,0.122411,0.098017,0.073565,0.049068,0.024541,0.0,-0.024541,-0.049068,-0.073565,-0.098017,-0.122411,-0.14673,-0.170962,-0.19509,-0.219101,-0.24298,-0.266713,-0.290285,-0.313682,-0.33689,-0.359895,-0.382683,-0.405241,-0.427555,-0.449611,-0.471397,-0.492898,-0.514103,-0.534998,-0.55557,-0.575808,-0.595699,-0.615232,-0.634393,-0.653173,-0.671559,-0.689541,-0.707107,-0.724247,-0.740951,-0.757209,-0.77301,-0.788346,-0.803208,-0.817585,-0.83147,-0.844854,-0.857729,-0.870087,-0.881921,-0.893224,-0.903989,-0.91421,-0.92388,-0.932993,-0.941544,-0.949528,-0.95694,-0.963776,-0.970031,-0.975702,-0.980785,-0.985278,-0.989177,-0.99248,-0.995185,-0.99729,-0.998795,-0.999699,-1.0,-0.999699,-0.998795,-0.99729,-0.995185,-0.99248,-0.989177,-0.985278,-0.980785,-0.975702,-0.970031,-0.963776,-0.95694,-0.949528,-0.941544,-0.932993,-0.92388,-0.91421,-0.903989,-0.893224,-0.881921,-0.870087,-0.857729,-0.844854,-0.83147,-0.817585,-0.803208,-0.788346,-0.77301,-0.757209,-0.740951,-0.724247,-0.707107,-0.689541,-0.671559,-0.653173,-0.634393,-0.615232,-0.595699,-0.575808,-0.55557,-0.534998,-0.514103,-0.492898,-0.471397,-0.449611,-0.427555,-0.405241,-0.382683,-0.359895,-0.33689,-0.313682,-0.290285,-0.266713,-0.24298,-0.219101,-0.19509,-0.170962,-0.14673,-0.122411,-0.098017,-0.073565,-0.049068,-0.024541]
     def __init__(self, i, f, a, d, s, r, k):
-        self.index = i              # Index of the current operator
-        self.f = f                  # frequency that the operator is running at 
-        self.a = a                  # attack time (in seconds)
-        self.d = d                  # decay time (in seconds)
-        self.s = s                  # sustain level (0.0 - 1.0)
-        self.r = r                  # release time (in seconds)
-        self.k = k                  # level / fm index
+        self.index = i                          # Index of the current operator
+        self.f = f                              # frequency that the operator is running at 
+        self.a = a                              # attack time (in seconds)
+        self.d = d                              # decay time (in seconds)
+        self.s = s                              # sustain level (0.0 - 1.0)
+        self.r = r                              # release time (in seconds)
+        self.k = k                              # level / fm index
 
     def __init__(self):
         self.index = 1
@@ -92,40 +85,22 @@ class Operator:
         self.k = 1.00
 
     def sineIndex(self, t):
-        phase =  (self.q4 * self.f * t)  % self.q4
-        i = phase * self.tableLength / self.q4
-        i = int(i * 4) % self.tableLength
-        output = 0.0
-        if phase < self.q1:
-            sinval = self.sinelut[i]
-            output = (self.explut[255-(sinval&0xFF)]+1024) >> (sinval>>8)
-        elif phase < self.q2:
-            sinval = self.sinelut[~i]
-            output = (self.explut[255-(sinval&0xFF)]+1024) >> (sinval>>8)
-        elif phase < self.q3:
-            sinval = self.sinelut[i]
-            output = -((self.explut[255-(sinval&0xFF)]+1024) >> (sinval>>8))
-        elif phase < self.q4:
-            sinval = self.sinelut[~i]
-            output = -((self.explut[(255-sinval&0xFF)]+1024) >> (sinval>>8))
-        output=output/2048
-        return output
+        return int(round(self.tableLength * (((self.f * t) % self.tau) / self.tau))) - 1
 
-    def sOscFM(self, t, note_on, note_off, msamp=0.0, mk=0.0):
-        # out = exp(logsin(phase2 + exp(logsin(phase1) + gain1)) + gain2)
-        unmod = self.sineIndex(t)
-        index = self.q4 * self.sineIndex(unmod) + (mk * msamp)
+    def sOscFM(self, t, note_on, note_off, msamp, mk):
+        # print(t,self.sineIndex(t))
+        index = self.tau * self.sine[self.sineIndex(t)] + (mk * msamp)
         index = int(index) % self.tableLength
-        return self.sAmp(t, note_on, note_off) * self.sineIndex(index)
+        return self.sAmp(t, note_on, note_off) * self.sine[index]
     
     def sOsc(self, t, note_on, note_off):
-        return self.sAmp(t, note_on, note_off) * self.sineIndex(t)
+        return self.sAmp(t, note_on, note_off) * self.sine[self.sineIndex(t)]
 
-    def sAmp(self, time):
+    def sAmp(self, time, note_on, note_off):
         amp = 0.0
-        l = time - self.note_on
+        l = time - note_on
 
-        if note_on > self.note_off:
+        if note_on > note_off:
             if l < self.a:                                                  # We are in the attack phase
                 amp = (l/self.a)                                            # Raise the amplitude to 1 over the course of the attack time
             
@@ -136,7 +111,7 @@ class Operator:
                 amp = self.d                                                # Maintain the sustain amplitude until the note is released
 
         else:                                                               # Note has been released, so we are in the release phase
-            amp = ((time-self.note_off) / self.r) * (0.0 - self.s) + self.s      # Reduce the amplitude to 0 over the course of the release time
+            amp = ((time-note_off) / self.r) * (0.0 - self.s) + self.s      # Reduce the amplitude to 0 over the course of the release time
 
         if amp < 0.0001:
             amp = 0.0                                                       # Amplitude should not be negative and should not be extremely small
@@ -148,7 +123,7 @@ class Operator:
         self.d = round(0.01 + random.random() * 2, 2)
         self.s = round(0.01 + random.random(), 2)
         self.r = round(0.1 + random.random() * 2, 2)
-        self.k = round(0.01 + random.random() * 12, 2)
+        self.k = round(0.01 + random.random() * 2000, 2)
 
     def dump(self):
         print("OP"+str(self.index)+":","F:"+str(self.f).rjust(7), "  K:"+str(self.k).rjust(8), '  ADSR:['+str(self.a).rjust(4), str(self.d).rjust(4), str(self.s).rjust(4), str(self.r).rjust(4)+']')
--- sine_wav.py
+++ sine_wav.py
@@ -4,6 +4,9 @@ import numpy as np
 import math
 from scipy.io import wavfile
 
+sinelut=np.array([2137, 1731, 1543, 1419, 1326, 1252, 1190, 1137, 1091, 1050, 1013, 979, 949, 920, 894, 869, 846, 825, 804, 785, 767, 749, 732, 717, 701, 687, 672, 659, 646, 633, 621, 609, 598, 587, 576, 566, 556, 546, 536, 527, 518, 509, 501, 492, 484, 476, 468, 461, 453, 446, 439, 432, 425, 418, 411, 405, 399, 392, 386, 380, 375, 369, 363, 358, 352, 347, 341, 336, 331, 326, 321, 316, 311, 307, 302, 297, 293, 289, 284, 280, 276, 271, 267, 263, 259, 255, 251, 248, 244, 240, 236, 233, 229, 226, 222, 219, 215, 212, 209, 205, 202, 199, 196, 193, 190, 187, 184, 181, 178, 175, 172, 169, 167, 164, 161, 159, 156, 153, 151, 148, 146, 143, 141, 138, 136, 134, 131, 129, 127, 125, 122, 120, 118, 116, 114, 112, 110, 108, 106, 104, 102, 100, 98, 96, 94, 92, 91, 89, 87, 85, 83, 82, 80, 78, 77, 75, 74, 72, 70, 69, 67, 66, 64, 63, 62, 60, 59, 57, 56, 55, 53, 52, 51, 49, 48, 47, 46, 45, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 23, 22, 21, 20, 20, 19, 18, 17, 17, 16, 15, 15, 14, 13, 13, 12, 12, 11, 10, 10, 9, 9, 8, 8, 7, 7, 7, 6, 6, 5, 5, 5, 4, 4, 4, 3, 3, 3, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0])
+explut=np.array([0, 3, 6, 8, 11, 14, 17, 20, 22, 25, 28, 31, 34, 37, 40, 42, 45, 48, 51, 54, 57, 60, 63, 66, 69, 72, 75, 78, 81, 84, 87, 90, 93, 96, 99, 102, 105, 108, 111, 114, 117, 120, 123, 126, 130, 133, 136, 139, 142, 145, 148, 152, 155, 158, 161, 164, 168, 171, 174, 177, 181, 184, 187, 190, 194, 197, 200, 204, 207, 210, 214, 217, 220, 224, 227, 231, 234, 237, 241, 244, 248, 251, 255, 258, 262, 265, 268, 272, 276, 279, 283, 286, 290, 293, 297, 300, 304, 308, 311, 315, 318, 322, 326, 329, 333, 337, 340, 344, 348, 352, 355, 359, 363, 367, 370, 374, 378, 382, 385, 389, 393, 397, 401, 405, 409, 412, 416, 420, 424, 428, 432, 436, 440, 444, 448, 452, 456, 460, 464, 468, 472, 476, 480, 484, 488, 492, 496, 501, 505, 509, 513, 517, 521, 526, 530, 534, 538, 542, 547, 551, 555, 560, 564, 568, 572, 577, 581, 585, 590, 594, 599, 603, 607, 612, 616, 621, 625, 630, 634, 639, 643, 648, 652, 657, 661, 666, 670, 675, 680, 684, 689, 693, 698, 703, 708, 712, 717, 722, 726, 731, 736, 741, 745, 750, 755, 760, 765, 770, 774, 779, 784, 789, 794, 799, 804, 809, 814, 819, 824, 829, 834, 839, 844, 849, 854, 859, 864, 869, 874, 880, 885, 890, 895, 900, 906, 911, 916, 921, 927, 932, 937, 942, 948, 953, 959, 964, 969, 975, 980, 986, 991, 996, 1002, 1007, 1013, 1018])
+
 sampleRate = 44100
 frequency = 440
 phase = -0.14 # to make this match up with the np.sin() based sine wave we are using for testing.
@@ -33,35 +36,45 @@ class EnvelopeGenerator:
         self.state = 'o' # o=off, a=attack, d=decay, s=sustain, r=release 
 
     def sAmp(self, time):
-        amp = 0.0
+        global explut
+        
+        amp = explut[255]
         l = time - self.note_on
         if l <= 0:
-            return 0.0
+            return 2048
         if l > 0:
-            if l < self.a:                                                  # We are in the attack phase
-                amp = (l/self.a)                                            # Raise the amplitude to 1 over the course of the attack time
-            
-            if l > self.a and l < (self.a + self.d):                        # We are in the decay phase
+            if l < self.a:                                                      # We are in the attack phase
+                factor = (l/self.a)
+                index = int(factor * 256)
+                index = min(255, index)
+                amp = explut[~index]
+
+            if l > self.a and l < (self.a + self.d):                            # We are in the decay phase
                 factor = (l - self.a) / self.d
-                amp = (1-factor) * (1-self.s) + self.s                      # Reduce the amplitude to the sustain amplitude over the course of the decay time 
+                low = int(self.s * 256)
+                diff = (255 - low)
 
-            if l > (self.a+self.d):                                         # We are in the sustain phase
-                amp = self.s                                                # Maintain the sustain amplitude until the note is released
-            
-            if time > self.note_off and l < self.note_off + self.r:            # Note has been released, so we are in the release phase
+                index = int(factor * diff)
+                index = min(255, index) 
+                amp = explut[index]
+
+            if l > (self.a+self.d):                                            # We are in the sustain phase
+                index = int(self.s * 256)
+                amp = explut[~index]
+
+            if time > self.note_off and time < (self.note_off + self.r):            # Note has been released, so we are in the release phase
                 factor = (time - self.note_off) / self.r
-                amp = self.s - (factor * self.s)                            # Reduce the amplitude to 0 over the course of the release time
-            if l >= self.note_off + self.r:
-                amp = 0.0
+                high = int(self.s * 256)
+                index = int((1.0-factor) * high)
+                index = abs(min(255,index))
+                amp = explut[~index]
+            
+            if time >= self.note_off + self.r:
+                amp = 2048
 
-        if amp < 0.0001:
-            amp = 0.0                                                       # Amplitude should not be negative and should not be extremely small
-        
         return amp
 
 class Operator:
-    sinelut=np.array([2137, 1731, 1543, 1419, 1326, 1252, 1190, 1137, 1091, 1050, 1013, 979, 949, 920, 894, 869, 846, 825, 804, 785, 767, 749, 732, 717, 701, 687, 672, 659, 646, 633, 621, 609, 598, 587, 576, 566, 556, 546, 536, 527, 518, 509, 501, 492, 484, 476, 468, 461, 453, 446, 439, 432, 425, 418, 411, 405, 399, 392, 386, 380, 375, 369, 363, 358, 352, 347, 341, 336, 331, 326, 321, 316, 311, 307, 302, 297, 293, 289, 284, 280, 276, 271, 267, 263, 259, 255, 251, 248, 244, 240, 236, 233, 229, 226, 222, 219, 215, 212, 209, 205, 202, 199, 196, 193, 190, 187, 184, 181, 178, 175, 172, 169, 167, 164, 161, 159, 156, 153, 151, 148, 146, 143, 141, 138, 136, 134, 131, 129, 127, 125, 122, 120, 118, 116, 114, 112, 110, 108, 106, 104, 102, 100, 98, 96, 94, 92, 91, 89, 87, 85, 83, 82, 80, 78, 77, 75, 74, 72, 70, 69, 67, 66, 64, 63, 62, 60, 59, 57, 56, 55, 53, 52, 51, 49, 48, 47, 46, 45, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 23, 22, 21, 20, 20, 19, 18, 17, 17, 16, 15, 15, 14, 13, 13, 12, 12, 11, 10, 10, 9, 9, 8, 8, 7, 7, 7, 6, 6, 5, 5, 5, 4, 4, 4, 3, 3, 3, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0])
-    explut=np.array([0, 3, 6, 8, 11, 14, 17, 20, 22, 25, 28, 31, 34, 37, 40, 42, 45, 48, 51, 54, 57, 60, 63, 66, 69, 72, 75, 78, 81, 84, 87, 90, 93, 96, 99, 102, 105, 108, 111, 114, 117, 120, 123, 126, 130, 133, 136, 139, 142, 145, 148, 152, 155, 158, 161, 164, 168, 171, 174, 177, 181, 184, 187, 190, 194, 197, 200, 204, 207, 210, 214, 217, 220, 224, 227, 231, 234, 237, 241, 244, 248, 251, 255, 258, 262, 265, 268, 272, 276, 279, 283, 286, 290, 293, 297, 300, 304, 308, 311, 315, 318, 322, 326, 329, 333, 337, 340, 344, 348, 352, 355, 359, 363, 367, 370, 374, 378, 382, 385, 389, 393, 397, 401, 405, 409, 412, 416, 420, 424, 428, 432, 436, 440, 444, 448, 452, 456, 460, 464, 468, 472, 476, 480, 484, 488, 492, 496, 501, 505, 509, 513, 517, 521, 526, 530, 534, 538, 542, 547, 551, 555, 560, 564, 568, 572, 577, 581, 585, 590, 594, 599, 603, 607, 612, 616, 621, 625, 630, 634, 639, 643, 648, 652, 657, 661, 666, 670, 675, 680, 684, 689, 693, 698, 703, 708, 712, 717, 722, 726, 731, 736, 741, 745, 750, 755, 760, 765, 770, 774, 779, 784, 789, 794, 799, 804, 809, 814, 819, 824, 829, 834, 839, 844, 849, 854, 859, 864, 869, 874, 880, 885, 890, 895, 900, 906, 911, 916, 921, 927, 932, 937, 942, 948, 953, 959, 964, 969, 975, 980, 986, 991, 996, 1002, 1007, 1013, 1018])
 
     def __init__(self):
         self.f = 440.0
@@ -81,10 +94,12 @@ class Operator:
         self.phase = (self.phase + self.phase_d) % tau
     
     def expValue(self, sinval):
-        return (self.explut[255-(sinval&0xFF)]+1024) >> (sinval>>8)
+        global explut
+        return (explut[255-(sinval&0xFF)]+1024) >> (sinval>>8)
  
 
     def sineValues(self, samples):
+        global sinelut
         global tableLength
         global tau
         global sampleRate
@@ -94,22 +109,24 @@ class Operator:
 
         sineIndex = self.phase * tableLength / tau
         sineIndex = int(sineIndex * 4) % tableLength
+        
+        amp = self.env.sAmp(samples)
+        amp = int(amp)
 
         if self.phase < np.pi / 2:
-            sinval = self.sinelut[sineIndex]
+            sinval = sinelut[sineIndex] + amp 
             output = self.expValue(sinval)
         elif self.phase < np.pi :
-            sinval = self.sinelut[~sineIndex]
+            sinval = sinelut[~sineIndex] + amp
             output = self.expValue(sinval)
         elif self.phase < 3 * np.pi / 2 :
-            sinval = self.sinelut[sineIndex]
+            sinval = sinelut[sineIndex] + amp
             output = -self.expValue(sinval)
         elif phase < tau:
-            sinval = self.sinelut[~sineIndex]
+            sinval = sinelut[~sineIndex] + amp
             output = -self.expValue(sinval)
         output = output/2048
-        amp = self.env.sAmp(samples)
-        output = output * amp
+        output = output
         return output
 
 op1 = Operator()
